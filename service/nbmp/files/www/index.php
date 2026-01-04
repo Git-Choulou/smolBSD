13fd962a2c801274a275349d40ea5d7830078791
Fab <245621518+Git-Choulou@users.noreply.github.com>
2026-01-04 02:45:43 +0100

    update index.php

--- service/nbmp/files/www/index.php
+++ service/nbmp/files/www/index.php
@@ -1,20 +1,42 @@
 <?php
+echo"
+<!DOCTYPE html>
+<html>
+<body><head>
+<style>
+
+.base{
+  border: solid 1px gray;
+  text-align:center;
+  width: max-content;
+  border-radius: 5px;
+  box-shadow: 5px 5px 5px gray;
+  padding: 0.5em;}
+
+#baseWrapper{
+  display:flex;
+  justify-content: space-around}
+
+li{text-align:left;}
+
+</style>
+</head>";
+
 echo "<h1>smolBSD 'nbmp' service</h1>
 <div style='font-size:small;'>(<b>N</b>etBSD <b>B</b>ozohttpd <b>M</b>ariadb <b>P</b>hp)</div>";
 echo "<h1 style='text-align:center;'>Example site</h1>";
 
-echo "<div style='text-align:center;'><a href='info.php'>See PHP info page</a>.</div>";
-echo "<img src='smolBSD.png' style='height:10em; display:block; margin:3em auto;' />";
+echo "<div style='text-align:center;'>See <a href='info.php'>PHP info page</a>.</div>";
+echo "<img src='smolBSD.png' style='height:10em; display:block; margin:1.5em auto;' />";
 
 
-// Connexion à MariaDB
 $mysqli = new mysqli("localhost", "root", "");
 
 if ($mysqli->connect_error) {
     die("Erreur de connexion : " . $mysqli->connect_error);
 }
 
-// Bases à exclure
+// Bases to exclude from display.
 $system_databases = [
     'mysql',
     'information_schema',
@@ -23,9 +45,10 @@ $system_databases = [
     'test'
 ];
 
-// Récupération des bases
+// Bases fetch.
 $result = $mysqli->query("SHOW DATABASES");
-
+echo "<div style='text-align:center; margin-bottom:1em;'>MariaDB content :</div>";
+echo "<div id='baseWrapper'>";
 while ($db = $result->fetch_row()) {
     $dbname = $db[0];
 
@@ -33,19 +56,19 @@ while ($db = $result->fetch_row()) {
         continue;
     }
 
-    echo "<h3>Base : $dbname</h3>";
+    echo "<div class='base'><div style='font-weight: bold;text-decoration: underline;'>$dbname</div>";
 
-    // Sélection de la base
     $mysqli->select_db($dbname);
 
-    // Récupération des tables
+    // Tables fetch.
     $tables = $mysqli->query("SHOW TABLES");
-    echo "<b>Tables :</b><br>";
+    echo "<ul>";
     while ($table = $tables->fetch_row()) {
-        echo $table[0] . "<br>";
+        echo "<li>" . $table[0] . "</li>";
     }
+    echo "</li></div>";
 }
 
 $mysqli->close();
-
+echo "</div></body></html>";
 ?>
